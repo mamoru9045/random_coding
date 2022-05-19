a2c4ec62ed996707251d99b297aeb0597ad38bec
mamoru9045 <91583016+mamoru9045@users.noreply.github.com>
2022-05-19 17:49:05 +0900

    Update random.vue
    無限ループ判定

--- random.vue
+++ random.vue
@@ -1,5 +1,6 @@
+<template>
     <div class="main">
-        <v-btn @click="random()" color="primary">
+        <v-btn @click="do_random()" color="primary">
             <v-icon>mdi-play</v-icon>シャッフル開始
         </v-btn>
     </div>
@@ -11,6 +12,16 @@ export default {
     data(){
     },
     methods: {
+        do_random(){
+            var ans_lists = this.random();
+
+            if(ans_lists[0]){
+                console.log(ans_lists[1])
+            }else{
+                this.do_random()
+            }
+        },
+
         random(){
         // グループリストは可変
         var GROUP_LISTS=[
@@ -28,6 +39,7 @@ export default {
             random_list.push(this.shuffle(shuffled))
             }
         while(ans_list.length<11){
+            // 群を重み付きランダマイズで取得
             var picked_index = this.ProbabilisticChoice(random_list);
             //  前回ans_listに入れた数字の群(リスト)を記録する。(群が連続することは許されない)
             var pre_picked_list = [];
@@ -35,16 +47,25 @@ export default {
                 if(l.includes(ans_list[ans_list.length - 1])){
                     pre_picked_list=l
                 }}
+
             var picked_num = random_list[picked_index][Math.floor(Math.random() * random_list[picked_index].length)];
             if(pre_picked_list.includes(picked_num)){
+                if(ans_list.length == 10){
+                    // 無限ループに入る
+                    console.log('無限ループ')
+                    return [false,null]
+                }
                 continue
             }else{
+                // console.log('pre_picked_list',pre_picked_list)
+                // console.log('picked_num',picked_num)
                 ans_list.push(picked_num)
+
                 var index = random_list[picked_index].findIndex(n => n === picked_num);
                 random_list[picked_index].splice(index, 1)
             }
         }
-        console.log(ans_list.slice(1))
+        return [true,ans_list.slice(1)]
         },
         // 単純なシャッフル
         shuffle(array){
